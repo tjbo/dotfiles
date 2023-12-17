454ed23f2fd0d5bc0a5b58a806c1e63c884789ac
tjbo <tom@prototypable.ca>
2023-12-17 14:27:00 -0500

    add android shell

---
+++ android/android.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+let
+  android-nixpkgs = callPackage <android-nixpkgs> { };
+
+  android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+    cmdline-tools-latest
+    build-tools-34-0-0
+    platform-tools
+    platforms-android-34
+    emulator
+  ]);
+
+in
+mkShell {
+  buildInputs = [
+    android-studio
+    android-sdk
+  ];
+}
