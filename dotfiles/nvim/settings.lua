b397615b601300a6ba855d041c1385c3c3e24396
tjbo <tom@prototypable.ca>
2024-01-14 15:34:53 -0500

    add mini move

--- dotfiles/nvim/settings.lua
+++ dotfiles/nvim/settings.lua
@@ -29,6 +29,26 @@ require("mini.comment").setup({
 		textobject = "gc",
 	},
 })
+require('mini.move').setup({
+	mappings = {
+		-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+		left = '<M-h>',
+		right = '<M-l>',
+		down = '<M-j>',
+		up = '<M-k>',
+
+		-- Move current line in Normal mode
+		line_left = '<M-h>',
+		line_right = '<M-l>',
+		line_down = '<M-j>',
+		line_up = '<M-k>',
+	},
+	-- Options which control moving behavior
+	options = {
+		-- Automatically reindent selection during linewise vertical move
+		reindent_linewise = true,
+	},
+})
 
 
 ----------------------------------------------------------------------
